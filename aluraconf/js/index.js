cb42e2fbb8d3aaef905898d0e550ee32af6e6774
jvitorthomaz <jovitorthomaz2@gmail.com>
2021-08-19 22:22:30 -0300

    commit full course

---
+++ aluraconf/js/index.js
@@ -0,0 +1,16 @@
+function scrollSuave(selector){
+    $(selector).click(function(event){
+
+        event.preventDefault();
+        var target = $(this).attr('href');
+
+        $('html, body').animate({
+            scrollTop: $(target).offset().top
+        }, 500)
+
+    });
+}
+
+scrollSuave('a[href*=panel-about]');
+scrollSuave('a[href*=panel-speakers]');
+scrollSuave('a[href*=panel-form]');
